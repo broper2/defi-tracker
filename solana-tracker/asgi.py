758dd92402a8ddd6bb20f6e4e063902e8962967b
broper2 <brianroper7@gmail.com>
2021-12-13 13:19:58 -0600

    update django project folder name

--- solana-tracker/asgi.py
+++ solana-tracker/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sol_tracker project.
+ASGI config for solana-tracker project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sol_tracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solana-tracker.settings')
 
 application = get_asgi_application()
