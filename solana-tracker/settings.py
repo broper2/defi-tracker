5f52db6f95ec50ae8d0cdfdcd196fc87b7bfe5b7
broper2 <brianroper7@gmail.com>
2021-12-13 16:44:36 -0600

    update settings for allowed hosts

--- solana-tracker/settings.py
+++ solana-tracker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://intense-brook-56676.herokuapp.com']
 
 
 # Application definition
