1702f66788e73711fb1b32341f8dc42b871eab13
broper2 <brianroper7@gmail.com>
2021-12-14 11:01:42 -0600

    refactor wallet tests

--- app/tests/unit/wallet.py
+++ app/tests/unit/wallet.py
@@ -20,7 +20,7 @@ class WalletTests(TestCase):
         self.client.force_login(self.user1)
         response = self.client.get('/wallets')
         expected_data = [
-            {'display_name': 'name1', 'sol': 2, 'usd': 4.0},
+            {'display_name': self.wallet1.display_name, 'sol': 2, 'usd': 4.0},
             {'display_name': 'Total', 'sol': 2, 'usd': 4.0},
         ]
         self.assertEquals(expected_data, response.context['data'])
@@ -37,7 +37,7 @@ class WalletTests(TestCase):
         self.client.force_login(self.user1)
         response = self.client.post('/wallets', {'wallet_pubkey': 'pubkey3', 'display_name': 'name3', 'user_id': 'user1'})
         expected_data = [
-            {'display_name': 'name1', 'sol': 2, 'usd': 4.0},
+            {'display_name': self.wallet1.display_name, 'sol': 2, 'usd': 4.0},
             {'display_name': 'name3', 'sol': 2, 'usd': 4.0},
             {'display_name': 'Total', 'sol': 4, 'usd': 8.0},
         ]
@@ -50,7 +50,7 @@ class WalletTests(TestCase):
         self.client.force_login(self.user1)
         response = self.client.post('/wallets', {'wallet_pubkey': 'invalid_pubkey', 'display_name': 'name4', 'user_id': 'user1'})
         expected_data = [
-            {'display_name': 'name1', 'sol': 2, 'usd': 4.0},
+            {'display_name': self.wallet1.display_name, 'sol': 2, 'usd': 4.0},
             {'display_name': 'Total', 'sol': 2, 'usd': 4.0},
         ]
         self.assertEquals(expected_data, response.context['data'])
