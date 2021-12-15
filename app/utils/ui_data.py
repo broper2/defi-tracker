8ccbb827e80023714f0a876a8576c0bd977a950d
broper2 <brianroper7@gmail.com>
2021-12-15 13:23:07 -0600

    refactor ui data helpers

--- app/utils/ui_data.py
+++ app/utils/ui_data.py
@@ -29,18 +29,21 @@ def get_wallet_table_data(wallet_adapters):
         return {}
     table_data = []
     for adapter in wallet_adapters:
-        table_data.append({
-            'display_name': adapter.display_name,
-            'sol': adapter.sol_value,
-            'usd': adapter.usd_value,
-        })
-    table_data.append(get_wallet_total_row_data(wallet_adapters))
+        table_data.append(_build_table_row(adapter.display_name, adapter.sol_value, adapter.usd_value))
+    table_data.append(_get_wallet_total_row_data(wallet_adapters))
     return table_data
 
 
-def get_wallet_total_row_data(wallet_adapters):
+def _get_wallet_total_row_data(wallet_adapters):
+    return _build_table_row(
+        'Total',
+        sum([adapter.sol_value for adapter in wallet_adapters]),
+        sum([adapter.usd_value for adapter in wallet_adapters]),
+    )
+
+def _build_table_row(name, sol, usd):
     return {
-        'display_name': 'Total',
-        'sol': round_sol(sum([adapter.sol_value for adapter in wallet_adapters])),
-        'usd': round_usd(sum([adapter.usd_value for adapter in wallet_adapters])),
+        'display_name': name,
+        'sol': round_sol(sol),
+        'usd': round_usd(usd),
     }
