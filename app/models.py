fa8dce019fa7dfae03815b4ceab477d29093323d
broper2 <brianroper7@gmail.com>
2021-12-18 14:26:22 -0600

    update wallet model with staked boolean

--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ class SolanaWallet(models.Model):
     wallet_pubkey = models.CharField(max_length=100)
     display_name = models.CharField(max_length=50)
     user_id = models.CharField(max_length=50)
+    staked = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.user_id} tracking wallet {self.display_name}'
