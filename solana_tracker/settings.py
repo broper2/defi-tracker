643c2f6fec72ebfc49c218319dfa2c6be7580d42
broper2 <brianroper7@gmail.com>
2021-12-20 15:59:57 -0600

    remove comment

--- solana_tracker/settings.py
+++ solana_tracker/settings.py
@@ -117,10 +117,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
