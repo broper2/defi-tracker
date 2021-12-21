f115b29ed89ea42347400ee880d2b7f5ff606550
broper2 <brianroper7@gmail.com>
2021-12-21 12:48:52 -0600

    update homepage layout

--- solana_tracker/settings.py
+++ solana_tracker/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'boot')
+    os.path.join(BASE_DIR, 'boot'),
+    os.path.join(BASE_DIR, 'jquery'),
 ]
 
 STATICFILES_FINDERS = [
