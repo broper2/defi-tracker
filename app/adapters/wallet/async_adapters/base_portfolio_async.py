ad8e716388917a135c57eef0174b28ce40c73402
broper2 <brianroper7@gmail.com>
2022-01-03 18:36:51 -0600

    rate limit consideration on async adapters

--- app/adapters/wallet/async_adapters/base_portfolio_async.py
+++ app/adapters/wallet/async_adapters/base_portfolio_async.py
@@ -1,6 +1,8 @@
 import asyncio
+import time
 
 from app.adapters.wallet.base_portfolio import DefiPortfolioAdapterBase
+from app.config.constants import SOLANA_SINGLE_RPC_RATE_LIMIT, SOLANA_RATE_LIMIT_TIMEOUT
 
 
 class DefiPortfolioAdapterAsyncBase(DefiPortfolioAdapterBase):
@@ -9,4 +11,18 @@ class DefiPortfolioAdapterAsyncBase(DefiPortfolioAdapterBase):
         return asyncio.run(self._get_child_composite_data())
 
     async def _get_child_composite_data(self):
-        return await asyncio.gather(*(child.get_component_data() for child in self._children))
+        child_composite_data = []
+        child_batches = self._get_child_batches()
+        timeout_required = False
+        for batch in child_batches:
+            if timeout_required:
+                time.sleep(SOLANA_RATE_LIMIT_TIMEOUT)
+            data = await asyncio.gather(*(child.get_component_data() for child in batch))
+            child_composite_data += data
+            timeout_required = True
+        return child_composite_data
+
+    def _get_child_batches(self):
+        num_children = len(self._children)
+        for index in range(0, num_children, SOLANA_SINGLE_RPC_RATE_LIMIT):
+            yield self._children[index:min(index + SOLANA_SINGLE_RPC_RATE_LIMIT, num_children)]
