aea5b6401a4bb6072c45152a33d40af6c8502c1a
broper2 <brianroper7@gmail.com>
2022-01-10 07:58:12 -0600

    use lru_cache kwargs

--- app/utils/timed_cache.py
+++ app/utils/timed_cache.py
@@ -8,7 +8,7 @@ def timed_cache(**timedelta_kwargs):
         update_delta = timedelta(**timedelta_kwargs)
         next_update = datetime.utcnow() + update_delta
         # Apply @lru_cache to f with no cache size limit
-        f = functools.lru_cache(None)(f)
+        f = functools.lru_cache(maxsize=None)(f)
 
         @functools.wraps(f)
         def _wrapped(*args, **kwargs):
